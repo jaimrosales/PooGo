36eec8e5b470bb96e6f43873c42dd3973eb24348
jaimrosales <jaimrosales@tesla.com>
2025-08-12 02:38:52 -0500

    Se agrega y el tema de type switch y se termina el curso de POO en GO

--- Polimorfismo/emptyInterface/main.go
+++ Polimorfismo/emptyInterface/main.go
@@ -12,6 +12,11 @@ package main
 // Las assertions o aserciones permiten validar el tipo concreto que tiene la interfas vacia,
 // para ingresar las inserciones se escribe el objeto de interface i seguido de un punto i. seguido del tipo que se quiere validar entre parentesis
 //i.(string) o i.(uint), las inserciones retornan el valor de la interface ademas de un bool valor, validacion := i.(uint)
+
+// En caso de que tengamos que validar distintos tipos de tipos, ya que las assertions solo nos permiten validar un tipo a la vas para no tener millar de if ok
+// se puede usar un tipe switch,
+//La forma en que se usa es con un siwtch normal y en variable se pondra una assertion pero utilizando type de modo switch v := i.(type){} y se evalua los casos que se busquen validar
+
 import (
 	"fmt"
 	"strings"
@@ -23,12 +28,24 @@ type exampler interface {
 
 func wrapper(i interface{}) {
 	fmt.Printf("valor: %v, Tipe:%T \n", i, i)
-
+	//ejemplo de insercion
 	v, ok := i.(string) //Se genera una insercion para validar si tipo string
-
 	if ok {
 		fmt.Printf("\t %s\n", strings.ToUpper(v))
 	}
+
+	//Ejemplo de type siwtch
+
+	switch v := i.(type) {
+	case string:
+		fmt.Printf("\t %s\n", strings.ToUpper(v))
+	case int:
+		fmt.Println(v * 2)
+	default:
+		fmt.Printf("valor: %v, Tipe:%T \n", i, i)
+
+	}
+
 }
 
 func main() {
